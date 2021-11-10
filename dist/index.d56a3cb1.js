170c607b21b159e18a6378ad9c6cbcaed0dbf4d3
nienkek2101 <nienke.kapers@gmail.com>
2021-11-10 20:59:42 +0100

    Ik loop vast op het koppelen van kleur aan landnaam

--- dist/index.d56a3cb1.js
+++ dist/index.d56a3cb1.js
@@ -480,8 +480,10 @@ async function countrySearch() {
         const allCountries = await _axiosDefault.default.get('https://restcountries.com/v2/all');
         const oneCountry = allCountries.data[5].name;
         const stringPopulation = `Has a population of ${allCountries.data[5].population} people`;
+        const urlFlag = allCountries.data[5].flag;
         return listOfCountries.innerHTML = `
         <li>
+            <img src="${urlFlag}" class="mini-flag">
             <h3>${oneCountry}</h3>
             <p>${stringPopulation}</p>
         </li>
@@ -491,7 +493,37 @@ async function countrySearch() {
         console.error(e);
     }
 }
-countrySearch();
+countrySearch(); /* Poging met afhankelijk van regio een kleur aan de naam van het land geven - ik liep vast */  // async function countrySearch() {
+ //     const listOfCountries = document.getElementById('list-of-countries');
+ //     try {
+ //         const allCountries = await axios.get('https://restcountries.com/v2/all');
+ //
+ //         const oneCountry = allCountries.data[5].name;
+ //         const stringPopulation = `Has a population of ${allCountries.data[5].population} people`;
+ //         const urlFlag = allCountries.data[5].flag;
+ //         const whatRegion = allCountries.data[5].region;
+ //         const isAfrica = "africa";
+ //         const isAmericas = "americas";
+ //         const isAsia = "asia";
+ //         const isEurope = "europe";
+ //         const isOceania = "oceania";
+ //
+ //         if (whatRegion === "africa") {
+ //             <h3 class="${isAfrica}">${oneCountry}</h3>
+ //         }
+ //
+ //         return listOfCountries.innerHTML = `
+ //         <li>
+ //             <img src="${urlFlag}" class="mini-flag">
+ //
+ //             <p>${stringPopulation}</p>
+ //         </li>
+ //         `;
+ //         // console.log(allCountries.data);
+ //     } catch(e) {
+ //         console.error(e);
+ //     }
+ // }
 
 },{"axios":"1IeuP","@parcel/transformer-js/src/esmodule-helpers.js":"ciiiV"}],"1IeuP":[function(require,module,exports) {
 module.exports = require('./lib/axios');
--- src/app.js
+++ src/app.js
@@ -18,10 +18,14 @@ async function countrySearch() {
     const listOfCountries = document.getElementById('list-of-countries');
     try {
         const allCountries = await axios.get('https://restcountries.com/v2/all');
+
         const oneCountry = allCountries.data[5].name;
         const stringPopulation = `Has a population of ${allCountries.data[5].population} people`;
+        const urlFlag = allCountries.data[5].flag;
+
         return listOfCountries.innerHTML = `
         <li>
+            <img src="${urlFlag}" class="mini-flag">
             <h3>${oneCountry}</h3>
             <p>${stringPopulation}</p>
         </li>
@@ -33,3 +37,38 @@ async function countrySearch() {
 }
 
 countrySearch();
+
+
+/* Poging met afhankelijk van regio een kleur aan de naam van het land geven - ik liep vast */
+
+// async function countrySearch() {
+//     const listOfCountries = document.getElementById('list-of-countries');
+//     try {
+//         const allCountries = await axios.get('https://restcountries.com/v2/all');
+//
+//         const oneCountry = allCountries.data[5].name;
+//         const stringPopulation = `Has a population of ${allCountries.data[5].population} people`;
+//         const urlFlag = allCountries.data[5].flag;
+//         const whatRegion = allCountries.data[5].region;
+//         const isAfrica = "africa";
+//         const isAmericas = "americas";
+//         const isAsia = "asia";
+//         const isEurope = "europe";
+//         const isOceania = "oceania";
+//
+//         if (whatRegion === "africa") {
+//             <h3 class="${isAfrica}">${oneCountry}</h3>
+//         }
+//
+//         return listOfCountries.innerHTML = `
+//         <li>
+//             <img src="${urlFlag}" class="mini-flag">
+//
+//             <p>${stringPopulation}</p>
+//         </li>
+//         `;
+//         // console.log(allCountries.data);
+//     } catch(e) {
+//         console.error(e);
+//     }
+// }
