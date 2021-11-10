62326c1443caa56d834c7f7450a93309c8f89132
nienkek2101 <nienke.kapers@gmail.com>
2021-11-10 15:26:37 +0100

    start project mbv filmpje

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+import axios from 'axios';
+
 console.log('Hallo daar!');
+
+async function fetchJoke() {
+    try {
+        const result = await axios.get('https://api.chucknorris.io/jokes/random');
+        console.log(result);
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+fetchJoke();
